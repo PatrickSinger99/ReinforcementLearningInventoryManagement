a091ed0e000dad1116fc1ef3a5e46a531d705e3a
Patrick Singer <patrick.singer1999@gmail.com>
2022-07-16 16:54:49 +0200

    graphs

--- x_graphs/stochastic_demand_and_lead_times.py
+++ x_graphs/stochastic_demand_and_lead_times.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 
-warehouse_1 = [0, 8, 6, 4, 2, 8, 6, 4, 2, 8, 6, 4, 2, 0, 0, 6, 4, 2, 8, 6, 4, 2, 0, 0, 6, 4, 2, 0, 6, 4, 2, 0, 6, 4, 10, 8, 6, 4, 2, 0, 0]
+warehouse_1 = [0, 9, 8, 7, 5, 10, 9, 6, 5, 10, 9, 8, 7, 5, 12, 11, 10, 9, 7, 5, 3, 10, 7, 4, 1, 0, 6, 5, 12, 11, 8, 5, 3, 1, 7, 6, 11, 9, 8, 5, 4]
 
-action_1 = [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0]
+action_1 = [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1]
 
 
 def convert_to_marker_pos(action):
@@ -23,14 +23,14 @@ plt.ylabel("Inventory Level")
 plt.xlabel("Round")
 plt.xlim([1, 40])
 plt.ylim([0, 30])
-plt.title("Simulation with stochastic lead times ranging from 1 to 3")
+plt.title("Simulation with stochastic lead times & stochastic demand")
 
 plt.tight_layout(pad=1)
 plt.show()
 
 """
-after 30000 training steps
-Demand 2, Lead time 1-3
+Both after 50000 training steps a 1250 generations
+Demand 1-3, Lead time 1-3
 Convegence
 PPO
 """
