535e3d996a7e0b4af03f60ca440da7609f7f66a5
Patrick Singer <patrick.singer1999@gmail.com>
2022-07-20 18:29:31 +0200

    new files for obs space testing

---
+++ x_graphs/prioritization.py
@@ -0,0 +1,74 @@
+from matplotlib import pyplot as plt
+import matplotlib.lines as mlines
+
+warehouse_1 = [0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0, 8, 6, 4, 2, 0]
+warehouse_2 = [0, 7, 4, 1, 8, 5, 12, 9, 6, 3, 0, 0, 0, 7, 4, 1, 0, 0, 0, 0, 0, 0, 7, 4, 1, 0, 0, 0, 0, 0, 0, 0, 7, 4, 1, 0, 0, 0, 0, 0, 0]
+warehouse_cw = [0, 20, 10, 20, 0, 0, 0, 0, 10, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+manufacturer = [0, 0, 3, 6, 9, 12, 5, 8, 1, 4, 7, 10, 3, 6, 9, 12, 15, 8, 1, 4, 7, 0, 3, 6, 9, 12, 15, 8, 1, 4, 7, 10, 3, 6, 9, 12, 15, 8, 1, 4, 7]
+
+action_1 = [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0]
+action_2 = [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+action_2_without_removed = [0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1]
+action_cw = [0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0]
+
+
+def convert_to_marker_pos(action):
+    return_list = []
+    i = 0
+    for entry in action:
+        if entry == 1:
+            return_list.append(i)
+        i += 1
+    return return_list
+
+
+reorder_marker = mlines.Line2D([], [], color='#66C2A5', marker='o', linestyle='None', label="Agent reorder point")
+reorder_marker_cw = mlines.Line2D([], [], color='#FC8D62', marker='o', linestyle='None', label="Agent reorder point")
+
+plt.rcParams["figure.figsize"] = (10, 7)
+
+plt.subplot(2, 2, 1)
+plt.plot(warehouse_1, "-bo", label="RW 1", markevery=convert_to_marker_pos(action_1), color="#66C2A5")
+plt.legend(handles=[reorder_marker])
+plt.ylabel("Inventory Level")
+plt.xlabel("Round")
+plt.xlim([1, 40])
+plt.ylim([0, 30])
+plt.title("Regional Warehouse 1")
+
+plt.subplot(2, 2, 2)
+plt.plot(warehouse_2, "-bo", label="RW 2", markevery=convert_to_marker_pos(action_2_without_removed), color="#66C2A5")
+plt.legend(handles=[reorder_marker])
+plt.ylabel("Inventory Level")
+plt.xlabel("Round")
+plt.xlim([1, 40])
+plt.ylim([0, 30])
+plt.title("Regional Warehouse 2")
+
+plt.subplot(2, 2, 3)
+plt.plot(warehouse_cw, "-bo", label="CW", markevery=convert_to_marker_pos(action_cw), color="#FC8D62")
+plt.legend(handles=[reorder_marker_cw])
+plt.ylabel("Inventory Level")
+plt.xlabel("Round")
+plt.xlim([1, 40])
+plt.ylim([0, 50])
+plt.title("Central Warehouse")
+
+plt.subplot(2, 2, 4)
+plt.plot(manufacturer, label="Manufacturer", color="#8DA0CB")
+plt.ylabel("Inventory Level")
+plt.xlabel("Round")
+plt.xlim([1, 40])
+plt.ylim([0, 50])
+plt.title("Manufacturer")
+
+
+plt.suptitle("Simulation with production deficit and prioritization", fontsize=15)
+plt.tight_layout(pad=1)
+plt.show()
+
+"""
+PPO
+lead time 2
+100000 training steps 
+"""
